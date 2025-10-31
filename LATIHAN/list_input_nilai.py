616600ac9d9e5658852608403f1987eb49b43176
Muhamad Akbar Ibrahim <kobsakbar@gmail.com>
2025-11-01 00:43:32 +0700

    Implement student grade status and use pandas
    Added status calculation for student grades and used pandas for data representation.

--- LATIHAN/list_input_nilai.py
+++ LATIHAN/list_input_nilai.py
@@ -1,7 +1,10 @@
+import pandas as pd
+
 list_nim = []
 list_uts = []
 list_uas = []
 list_total = []
+list_status = []
 
 
 banyak_data = int(input("Masukan Jumlah data yang ingin anda masukan: "))
@@ -13,12 +16,23 @@ for i in range(banyak_data):
     list_uts.append(int(input("Masukan Nilai UTS Anda : ")))
     list_uas.append(int(input("Masukan Nilai UAS Anda : ")))
 print("\n*********************************************")
-
-
-print("===============================================================")
-print("|          NIM          |  NILAI UTS  |  NILAI UAS  |  TOTAL  |")
-print("===============================================================")
 for i in range(banyak_data):
-    list_total.append(int(list_uts[i] + list_uas[i])/2)
-    print(f"|        {list_nim[i]}       |      {list_uts[i]}     |     {list_uas[i]}      |   {list_total[i]}  |")
-print("===============================================================")
+    total_nilai = int(list_uts[i] + list_uas[i])/2
+    list_total.append(total_nilai)
+    if total_nilai >= 70:
+        list_status.append("Lulus")
+    else:
+        list_status.append("Tidak Lulus")
+
+siswa = {
+    "NIM" : list_nim,
+    "Nilai UTS": list_uts,
+    "Nilai UAS": list_uas,
+    "Rata-rata": list_total,
+    "Status": list_status
+}
+
+data_siswa = pd.DataFrame(siswa)
+print("============== Daftar Nilai Siswa =============")
+print(data_siswa)
+print("===============================================")
